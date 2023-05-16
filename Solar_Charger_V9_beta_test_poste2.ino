f4e53b30943bfd535a3000b04ebb6950c61b0da1
PepLPZ <122974244+PepLPZ@users.noreply.github.com>
2023-05-16 09:24:03 +0200

    Update Solar_Charger_V10_beta_test_poste3.ino
    CUTOFF LOAD

--- Solar_Charger_V9_beta_test_poste2.ino
+++ Solar_Charger_V9_beta_test_poste2.ino
@@ -1,29 +1,37 @@
-////////////////////////////////PWM SOLAR CHARGE CONTROLLER V-9.0 NO LCD POSTE 2/////
-//----------------------------------------------------------------------------------
+////////////////PWM SOLAR CHARGE CONTROLLER V-10.1 POSTE 3/////
+  //-----------------------------------------------------------------------------
+  //
   //  Author: JOSEP LOPEZ SANCHEZ
   //          www.ea3abn.com
-  //  This code is for an arduino based Solar PWM charge controller ( V-2.0)
-  //  Credit :logic of 3 stage charging taken from https://github.com/PepLPZ/solarCharger/blob/main/Solar_Charger_V7_beta_test.ino
-  //  Last updated on 09/05/2023
-  //-----------------------------------------------------------------------------------
-  //----------------------------------- Arduino pins Connections------------------------------
+  //  This code is for an arduino based Solar PWM charge controller by ea3abn@2010
+  //  Credit :logic of 3 stage charging taken from https://github.com/PepLPZ/solarCharger/
+  //  Last updated on 10/05/2023
+  //  He añadido la deteccion del estado del amplificador ON/OFF en el PIN D1--- 
+  //  pendiente de realizar la maniobra tan solo esta definido
+  //  Previsto ajustar el valor del sensor de intensidad de consumo de la carga.
+  //  Previsto poner una proteccion para desconectar la salida de la bateria hacia la carga, 
+  //  cuando el voltaje baje de los 10,2V
+  //  40 grados Celsius se toma como temperatura ambiente estándar, debido a la estanqueidad de la caja y
+  //  a la exposicion continua al sol,se determina ese valor (Aún en pruebas)
+  //------------------------------------------------------------------------------
+  //----------------------------------- Arduino pins Connections------------------
   // A0 - Voltage divider to measure solar panel voltage
   // A1 - Voltage divider to measure battery voltage
   // A2 - ACS712 to monitor load current
   // A3 - ACS712 to monitor solar current
   // A4 - LCD SDA 
   // A5 - LCD SCL
-  // D0 - Not used
-  // D1 - Not used
-  // D2 - Control Load MOSFET Q2 
-  // D3 - PWM Output to control MOSFET Q1 
-  // D4 - Push Switch Control LOAD (ADDED)<<<<<<<<<<<<<<<
+  // D0 - NO USADA  puerto serie no usar
+  // D1 - NO USADA  puerto serie no usar
+  // D2 - Salida de Control del Load MOSFET Q2 
+  // D3 - Salida de Control PWM MOSFET Q1 
+  // D4 - Push Switch Control LOAD
   // D5 - Battery Red LED       
   // D6 - Battery Green LED 
   // D7 - Battery Blue LED 
   // D8 - Load Orange Led 
   // D9 - Load Green Led  
-  // D10- Solar Red LED
+  // D10- ENTRADA DETECCION DE ESTADO DEL AMPLIFICADOR (1 ON)(0 OFF).
   // D11- Solar Green LED  
   // D12- DS18B20 Temp. Sensor
   // D13-not used  
@@ -31,28 +39,28 @@
 #include <OneWire.h>  
 #include <DallasTemperature.h> 
 
-#define SOL_ADC A0     // Solar panel side voltage divider is connected to pin A0 
-#define BAT_ADC A1     // Battery side voltage divider is connected to pin A1
+#define SOL_ADC A0        // Solar panel side voltage divider is connected to pin A0 
+#define BAT_ADC A1        // Battery side voltage divider is connected to pin A1
 #define LOAD_CURRENT_ADC A2  // ACS 712 current sensor is connected to pin A2 for load curremt
 #define SOL_CURRENT_ADC A3  // ACS 712 current sensor is connected to pin A3 for solar current
-#define AVG_NUM 10    // number of iterations of the adc routine to average the adc readings
-#define BAT_MIN 10.5  // minimum battery voltage for 12V system "CHANGED default BAT_MIN 10.5"
-#define BAT_MAX 14.6  // maximum battery voltage for 12V system "CHANGED default BAT_MAX 15.0"
-#define BULK_CH_SP 14.6 // bulk charge set point for sealed lead acid battery // flooded type set it to 14.6V
+#define AVG_NUM 10        // number of iterations of the adc routine to average the adc readings
+#define BAT_MIN 10.2      // minimum battery voltage for 12V system "CHANGED default BAT_MIN 10.2"
+#define BAT_MAX 14.6      // maximum battery voltage for 12V system "CHANGED default BAT_MAX 15.0"
+#define BULK_CH_SP 14.6   // bulk charge set point for sealed lead acid battery // flooded type set it to 14.6V
 #define FLOAT_CH_SP 13.6  //float charge set point for lead acid battery
-#define LVD 11.5       //Low voltage disconnect setting for a 12V system default (11.5)
-#define PWM_PIN 3      // pin-3 is used to control the charging MOSFET //the default frequency is 490.20Hz
-#define LOAD_PIN 2     // pin-2 is used to control the load
-#define LOAD_SWITCH 4  // pin-4 is used to SWICH ON OR OFF THE LOAD (PEP)<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-#define BAT_RED_LED 5
-#define BAT_GREEN_LED 6
-#define BAT_BLUE_LED 7
-#define LOAD_ORANGE_LED 8
-#define LOAD_GREEN_LED 9
-#define SOL_RED_LED 10
-#define SOL_GREEN_LED 11
-#define ONE_WIRE_BUS 12 // Datos del DS18B20 sensor de temperatura en el pin D12
-#define TRANSMISSION_INTERVAL 25000 // Defino el intervalo de tiempo que se enviarán las tramas LoRa 60000=1 Min. 300000=5 min.
+#define LVD 11.5          //Low voltage disconnect setting for a 12V system default (11.5)
+#define LOAD_PIN 2        // pin-2 is used to control the LOAD
+#define PWM_PIN 3         // pin-3 is used to control the charging MOSFET //the default frequency is 490.20Hz
+#define LOAD_SWITCH 4     // pin-4 is used to SWICH ON OR OFF THE LOAD (PEP)<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+#define BAT_RED_LED 5     // LED Rojo indicacion de la bateria
+#define BAT_GREEN_LED 6   // LED Verde indicacion de la bateria
+#define BAT_BLUE_LED 7    // LED Azul indicacion de la bateria
+#define LOAD_ORANGE_LED 8 // LED Naranja indicacion de la salida Carga Desactivada
+#define LOAD_GREEN_LED 9  // LED Verde indicacion de la salida Carga Activada
+#define AMPLI_PIN 10      // pin-1 is used to control the AMPLI ON/OFF
+#define SOL_GREEN_LED 11  // LED Verde indicacion de Entrada de voltaje desde el panel solar
+#define ONE_WIRE_BUS 12   // Datos del DS18B20 sensor de temperatura en el pin D12
+#define TRANSMISSION_INTERVAL 20000 // Defino el intervalo de tiempo que se enviarán las tramas LoRa 60000=1 Min. 300000=5 min.
 //----------------DECLARACION DE TODAS LAS VARIABLES GLOBALES--------------------------
 float solar_volt=0;
 float bat_volt=0;
@@ -69,7 +77,7 @@ int charge_stage =0;
 int load_status=0;
 double PWM_Duty =0; // PWM_Duty varies from 0 to 255
 float lvd; // <<<<<<<<<<<<<<<<Almacenamos el valor del voltage minimo de desconexion para bateria de 12v
-int Biestable=0; // VARIABLE PARA LA CONMUTACION del Switch ON/OFF
+
 ////////////////////////////// Variables Para las Medidas de Potencia y Energia
 long unsigned time =0; 
 long unsigned msec=0;
@@ -77,20 +85,16 @@ long unsigned last_msec=0;
 long unsigned elasped_msec=0;
 long unsigned elasped_time=0;
 unsigned long lastTransmissionTime = 0;
-bool sendData = true;// sendData se usa para enviar datos solo cuando esta variablees true. 
-                     //Una vez que se envía la trama de datos, se establece 
-                     //sendData en false para evitar envíos duplicados.
+bool sendData = true;   // sendData se usa para enviar datos solo cuando esta variablees true. 
+                        // Una vez que se envía la trama de datos, se establece 
+                        // sendData en false para evitar envíos duplicados.
 float load_ampSecs = 0;
 float load_ampHours=0;
-float load_watts=0;
-float load_wattSecs = 0;
-float load_wattHours=0;
 float solar_ampSecs = 0;
 float solar_ampHours=0;
-float solar_watts=0;
-float solar_wattSecs = 0;
-float solar_wattHours=0;
-
+bool estadoCarga = true;  // Variable global para el estado de carga, inicializada a true (carga activada)
+int estadoAmpli = 0;      // Variable global para el estado de carga, inicializada a 0 (Ampli apagado)
+float low_Bat=0;          // Variable para almacenar si se ha disparado el fail_safe por low voltage de la bateria
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance to communicate with any OneWire devices
 DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature sensor 
 //********************************* MAIN PROGRAM START ************************
@@ -99,7 +103,7 @@ void setup()
 Serial.begin(9600);
 sensors.begin(); // // Start up the library  
 TCCR2B = TCCR2B & B11111000 | 0x03;    // pin 3 PWM frequency of 980.39 Hz
-pinMode(SOL_RED_LED ,OUTPUT);
+//pinMode(SOL_RED_LED ,OUTPUT);
 pinMode(SOL_GREEN_LED,OUTPUT);
 pinMode(BAT_RED_LED,OUTPUT);
 pinMode(BAT_GREEN_LED,OUTPUT);
@@ -108,27 +112,32 @@ pinMode(LOAD_ORANGE_LED ,OUTPUT);
 pinMode(LOAD_GREEN_LED,OUTPUT);
 pinMode(PWM_PIN,OUTPUT);
 pinMode(LOAD_PIN,OUTPUT);   //<<<<< SALIDA DE CARGA
-pinMode(LOAD_SWITCH,INPUT); // Define pinD4 input to control the load ON/OFF <<<<< PULSADOR CARGA
+pinMode(AMPLI_PIN ,INPUT); //configuro el pin 1 como entrada de deteccion del ampli ON/OFF
+pinMode(LOAD_SWITCH,INPUT); // Define pin D4 input to control the load ON/OFF mediante PULSADOR CARGA o fail safe
 digitalWrite(PWM_PIN,LOW);  // default value of pwm duty cycle
 digitalWrite(LOAD_PIN,HIGH);  // default load state is ON CADA VEZ QUE ARRANCAMOS
+// Actualizar el estado de los LED indicadores de carga en el inicio
+digitalWrite(LOAD_ORANGE_LED, !estadoCarga); // Carga desactivada -> encender LED rojo
+digitalWrite(LOAD_GREEN_LED, estadoCarga); // Carga activada -> encender LED verde
+
 }
 void loop()
 {
- load_Push();             // Lectura del pulsador
- read_data();             // read different sensors data from analog pin of arduino
- system_voltage();        // detect the system voltage according to battery voltage
- setpoint();              // decide the charge set point according to system voltage
- charge_cycle();          // pwm charging of battery
- led_indication();        // led indication
-      // Envía tramas de telemetría sólo si ha pasado un cierto tiempo desde la última transmisión
-      //     if (millis() - lastTransmissionTime >= TRANSMISSION_INTERVAL) { // envía trama cada 5 minutos (300000 ms)
-      //       print_data();
-      //         lastTransmissionTime = millis();
- if (millis() - lastTransmissionTime >= TRANSMISSION_INTERVAL) {
-    sendData = true;
-}
-
-if (sendData) {
+ load_Push();         // Lectura del pulsador de la salida LOAD
+ load_led();          // Actualizar estado de los LED en función del estado de la carga
+ read_data();         // read different sensors data from analog pin of arduino
+ system_voltage();    // detect the system voltage according to battery voltage
+ setpoint();          // decide the charge set point according to system voltage
+ charge_cycle();      // pwm charging of battery
+ led_indication();    // led indication
+ fail_safe();         // Modo Fail Safe, desconectamos la salida de la carga si la bateria baja por debajo de los 10.5V
+ Ampli_Push();        // Deteccion estado amplificador
+ 
+  if (millis() - lastTransmissionTime >= TRANSMISSION_INTERVAL) { // Envía tramas de telemetría sólo si ha pasado un cierto tiempo desde la última transmisión
+    sendData = true;                                              // if (millis() - lastTransmissionTime >= TRANSMISSION_INTERVAL) { // envía trama cada 5 minutos (300000 ms)
+}                                                                 // print_data();
+                                                                  // lastTransmissionTime = millis();
+  if (sendData) {
     print_data();
     lastTransmissionTime = millis();
     sendData = false;
@@ -142,36 +151,52 @@ int read_adc(int adc_parameter)
   int sum = 0;
   int sample ;
   for (int i=0; i<AVG_NUM; i++) 
-  {                                        // loop through reading raw adc values AVG_NUM number of times  
-    sample = analogRead(adc_parameter);    // read the input pin  
-    sum += sample;                        // store sum for averaging
-    delayMicroseconds(50);              // pauses for 50 microseconds  
+  {                                      // loop through reading raw adc values AVG_NUM number of times  
+    sample = analogRead(adc_parameter);  // read the input pin  
+    sum += sample;                       // store sum for averaging
+    delayMicroseconds(50);               // pauses for 50 microseconds  
   }
-  return(sum / AVG_NUM);                // divide sum by AVG_NUM to get average and return it
+  return(sum / AVG_NUM);                 // divide sum by AVG_NUM to get average and return it
 }
-///////////////////////////////////LOAD CONTROL SWITCH ON/OFF BY PP///////////////////////// 
+///////////////////////////////////LOAD CONTROL SWITCH ON/OFF ///////////////////////// 
 void load_Push(void)
-{  
-  // Encender la carga si pulsamos el pulsador y la variable Biestable es igUal a cero  
+{
+  if (digitalRead(LOAD_SWITCH) == HIGH) // Pulsador de carga presionado
   {
- if (digitalRead(LOAD_SWITCH)==HIGH && Biestable==0 && load_status==0)
+    // Encender la carga si está apagada
+    if (estadoCarga == 0)
+    {
+      estadoCarga = 1; // Cambiar estado de carga
+      digitalWrite(LOAD_PIN, HIGH); // Encender carga
+    }
+    // Apagar la carga si está encendida
+    else if (estadoCarga == 1)
+    {
+      estadoCarga = 0; // Cambiar estado de carga
+      digitalWrite(LOAD_PIN, LOW); // Apagar carga
+    }
+    delay(10); // Esperar un corto tiempo para evitar rebotes en el pulsador
+  }
+}
+/////////////////////////////////////AMPLI STATUS DETECTOR ON/OFF ///////////////////////// 
+void Ampli_Push(void)
+{
+  if (digitalRead(AMPLI_PIN) == HIGH) // Ampli ON
   {
-   Biestable=1;
-   load_status=1;  
-   digitalWrite(LOAD_PIN, HIGH); // load is ON
-   delay(10);
+    // Poner a ON la variable si esta apagada
+    if (estadoAmpli == 0)
+    {
+      estadoAmpli = 1; // Cambiar estado de la variable
+    }
+    // Poner a off la variable si esta encendida
+    else if (estadoAmpli == 1)
+    {
+      estadoAmpli = 0; // Cambiar estado de carga
+    }
+    delay(10); // Esperar un corto tiempo para evitar rebotes en el pulsador
   }
-  // Apagar LED si pulsamos el pulsador y la variable biestable es igUal a uno
-   else if (digitalRead(LOAD_SWITCH)==HIGH && Biestable==1 && load_status==1)
-  {
-   Biestable=0;// Cambiamos valor de variable a 0
-   load_status=0; 
-   digitalWrite(LOAD_PIN, LOW); //load is OFF
-   delay(10);
-  }
-  }
-} 
-////////////////////////////////////READ THE DATA/////////////////////////////////////////////
+}
+////////////////////////////////////OBTENER LECTURAS DE SENSADO/////////////////////////////////////////
  void read_data(void) 
  {
    // 5V = ADC value 1024 => 1 ADC value = (5/1024)Volt= 0.0048828Volt
@@ -179,7 +204,12 @@ void load_Push(void)
     
      solar_volt = read_adc(SOL_ADC)*0.00500*(120/20);/// valor por defecto 0.00488 valor corregido *0.00540*
      bat_volt   = read_adc(BAT_ADC)*0.00490*(120/20);/// valor por defecto 0.00488 valor corregido *0.00580*
-     load_current = ((read_adc(LOAD_CURRENT_ADC)*0.00488 - offsetVoltage -0.05)/Sensitivity ); // 2.4V offset when no load is connected
+     //
+     //
+     //    load_current = ((read_adc(LOAD_CURRENT_ADC)*0.00488 - offsetVoltage -0.05)/Sensitivity ); // 2.4V offset when no load is connected
+     load_current = ((read_adc(LOAD_CURRENT_ADC)*0.00502 - offsetVoltage -0.05)/Sensitivity );// VALORES MODIFICADOS PARA INDICAR CORRECTAMENTE
+     //
+     //
      solar_current = ((read_adc(SOL_CURRENT_ADC)*0.00488 - offsetVoltage)/ Sensitivity ); 
       if (load_current <0)
      {
@@ -200,20 +230,20 @@ void print_data(void)
   delay(10); // Agrega una pausa para evitar enviar datos demasiado rápido
 
   // Envía el voltaje solar
-  Serial.print("AirVoice2/");
-  Serial.print("poste2");
+  Serial.print("AirVoice3/");
+  Serial.print("poste3");
   Serial.print("/sol/volt/");  
   Serial.println(solar_volt);
   delay(1400);
   // Envía intensidad de carga desde el panel solar
-  Serial.print("AirVoice2/");
-  Serial.print("poste2");
+  Serial.print("AirVoice3/");
+  Serial.print("poste3");
   Serial.print("/sol/chge/");
   Serial.println(solar_current);
   delay(1400);
   // Envía el voltaje de la batería
-  Serial.print("AirVoice2/");
-  Serial.print("poste2");
+  Serial.print("AirVoice3/");
+  Serial.print("poste3");
   Serial.print("/bat/volt/"); 
   Serial.println(bat_volt);
   delay(1400);
@@ -224,10 +254,16 @@ void print_data(void)
   Serial.println(temperature);
    delay(1400);
   // Envía el valor del consumo de la carga
-  Serial.print("AirVoice2/");
-  Serial.print("poste2");
+  Serial.print("AirVoice3/");
+  Serial.print("poste3");
   Serial.print("/load/amp/");
-  Serial.println(load_current); 
+  Serial.println(load_current);
+   delay(1400); 
+  // Envia el status del Amplificador de audio
+  Serial.print("AirVoice3/");
+  Serial.print("poste3");
+  Serial.print("/stat/amp/");
+  Serial.println(estadoAmpli); 
  }
 //////////////////////////////////SYSTEM VOLTAGE AUTO DETECT /////////////////////////////////////
 void system_voltage(void)
@@ -240,7 +276,7 @@ void system_voltage(void)
 ////////////////////////////////////CHARGE SET POINT //////////////////////////////////////////////
  void setpoint(void)
 {
-  temp_change =temperature-25.0; // 25 grados Celsius se toma como temperatura ambiente estándar
+  temp_change =temperature-40.0; // 40 grados Celsius se toma como temperatura ambiente estándar
   // Compensacion de Temperatura = -5mv/degC/Cell 
   // Si la temperatura es superior a la temperatura ambiente; el punto de carga debe reducirse.
   // Si la temperatura está por debajo de la temperatura ambiente, el punto de ajuste de carga debe incrementarse.
@@ -363,23 +399,38 @@ void led_indication(void)
   load_led();              //Load led indication
 }
 ////////////////////////////SOLAR LED INDICATION//////////////////
-  
   void solar_led()
-  
   {
     if (solar_volt > float_charge_sp)
   {  
-      digitalWrite(SOL_RED_LED,LOW);
       digitalWrite(SOL_GREEN_LED,HIGH);  // Sun Light is available and charger is ready for charging
   } 
   else 
   {
-      digitalWrite(SOL_GREEN_LED,LOW);
-      digitalWrite(SOL_RED_LED,HIGH);  //Sun light is not available for charging
-      
+      digitalWrite(SOL_GREEN_LED,LOW);     
   }  
 }
-/////////////////////////////////////////////BATTERY LED INDICATION////////////////////
+//--------------------------------------------------------------------
+/////FAIL SAFE DESCONEXION DE LA SALIDA A LA CARGA CUANDO 
+///  LA BATERIA ESTÉ ENTRE (10,2V Y 12,7V)                    ////
+//--------------------------------------------------------------------
+void fail_safe(void)
+{
+    if (bat_volt >= 10.2) // carga activada
+    //low_Bat=0;
+    
+    {
+        digitalWrite(LOAD_PIN, HIGH);
+    }
+    else // carga desactivada
+    {
+        digitalWrite(LOAD_PIN, LOW);
+    //low_Bat=1;
+    }
+  
+
+}
+//////////////////////////////////////BATTERY LED INDICATION////////////////////
 void battery_led(void)
 {
   
@@ -400,20 +451,20 @@ void battery_led(void)
   }
 }
 //////////////////////////LOAD LED INDICATION///////////////
-   void load_led()
-
- {
-    if(load_status==1)
+void load_led()
+{
+    if (estadoCarga == true) // carga activada
     {
-      digitalWrite(LOAD_ORANGE_LED, LOW);
-      digitalWrite(LOAD_GREEN_LED,HIGH);
+        digitalWrite(LOAD_ORANGE_LED, LOW);
+        digitalWrite(LOAD_GREEN_LED, HIGH);
     }
-    else if(load_status==0)
+    else // carga desactivada
     {
-      digitalWrite(LOAD_GREEN_LED, LOW);
-      digitalWrite(LOAD_ORANGE_LED,HIGH);
+        digitalWrite(LOAD_GREEN_LED, LOW);
+        digitalWrite(LOAD_ORANGE_LED, HIGH);
     }
 }
+
 //////////////////////// TURN OFF ALL THE battery LEDs ////////////////
 void leds_off_all(void)
 
